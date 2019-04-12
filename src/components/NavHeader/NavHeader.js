a0070c0220561046cb6d3289f05cc10e5c64a8ad
Nev Flynn <nev@recroot.ie>
2019-04-12 15:53:27 +0100

    Added header

--- src/components/NavHeader/NavHeader.js
+++ src/components/NavHeader/NavHeader.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import '../../App.css';
 
+import styles from '../../components/NavHeader/NavHeader.module.css';
+
 class NavHeader extends Component {
     render(){
         return(
-            <div></div>
+            <div className={styles.navHeader}>
+                <h2>Huego.</h2>
+                <div className={styles.navHeaderLinks}>
+                    <h5 className={styles.navHeaderLink}>Profile</h5>
+                    <h5 className={styles.navHeaderLink}>Log Out</h5>
+                </div>
+            </div>
         )
     }
 }
